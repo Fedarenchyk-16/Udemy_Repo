cf628f223a5a7e20ac958c0fd9852140bac4c9f0
Nikita <fnikita01@mail.ru>
2021-08-11 23:28:20 +0300

    feat: add first functionality

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 "use strict";
 
-let number = 5;
-console.log(number);
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "1");
+console.log(numberOfFilms);
 
-const obj = {
-    
-}
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}; 
+
+let film,
+    mark;
+
+film = prompt("Один из последних просмотренных фильмов?", "1");
+mark = prompt("На сколько оцените его?", "1");
+
+let questionArray = [film,mark];
+
+let movie = {};
+movie[film] = mark;
+
+console.log(movie);
